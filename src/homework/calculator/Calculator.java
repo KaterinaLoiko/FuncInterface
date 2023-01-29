0692ce530de7d56497160be014493123c3f7506c
ekaterinalojko <eloiko@energocomm.ru>
2023-01-29 15:18:53 +0300

    add comment to Calculator

--- src/homework/calculator/Calculator.java
+++ src/homework/calculator/Calculator.java
@@ -11,7 +11,8 @@ public class Calculator {
   BinaryOperator<Integer> plus = (x, y) -> x + y;
   BinaryOperator<Integer> minus = (x, y) -> x - y;
   BinaryOperator<Integer> multiply = (x, y) -> x * y;
-  BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
+  //невозможно делить на 0, поэтому необходима проверка
+  BinaryOperator<Integer> divide = (x, y) -> y != 0 ? x / y : 0;
   UnaryOperator<Integer> pow = x -> x * x;
   UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
   Consumer<Integer> println = System.out::println;
